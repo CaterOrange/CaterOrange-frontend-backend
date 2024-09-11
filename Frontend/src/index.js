acb640a813a4b1b0b55313160b2057454968dbba
Sirisha-env <sirisha@scaleorange.com>
2024-09-11 20:27:11 +0530

    login,register,forgotpassword with integreation

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,17 +1,13 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="230275199603-3a6c8rmdomfrsr5mdhdl8pol04ubo44s.apps.googleusercontent.com">
+      <App/>
+      </GoogleOAuthProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
