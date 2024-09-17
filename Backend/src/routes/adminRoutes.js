452ccc6943fd6731dfb051b7fea629bc9c5f5d37
puneethk-scaleorange <puneeth@scaleorange.com>
2024-09-17 11:39:41 +0530

    Update adminRoutes.js

--- Backend/src/routes/adminRoutes.js
+++ Backend/src/routes/adminRoutes.js
@@ -3,10 +3,7 @@ const router = express.Router()
 const adminController = require('../controller/adminController');
 
 
-router.post('/categories', adminController.addCategory);
-router.post('/groups', adminController.createGroup);
-router.delete('/groups/:groupId', adminController.deleteGroup);
-router.get('/getallgroups', adminController.getAllGroups);
+
 // router.get('/api/customers/:id',adminController.getCustomerById)
 
 
