807f40d165733901e2b9d0f388942e26255f2c7f
srivallika-scaleorange <srivallika@scaleorange.com>
2024-10-29 19:52:04 +0530

    Update adminRoutes.js

--- Backend/src/routes/adminRoutes.js
+++ Backend/src/routes/adminRoutes.js
@@ -106,12 +106,8 @@ const typeDefs = gql`
     category_name: String,
     price_category: String,
     isdual: Boolean,
-    plate_units: String,
     priceperunit: Float,
-    minunitsperplate: Int,
-    wtorvol_units: String,
-    price_per_wtorvol_units: Float,
-    min_wtorvol_per_plate: Int
+    minunitsperplate: Int
   }
 
 
