dc117764de6348eda27aa4b7a3bab1d4f6c512ee
ParashDeveloper <parash@scaleorange.com>
2024-11-15 10:49:48 +0530

    updated payment url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -105,7 +105,7 @@ app.post("/api/pay", async(req, res) => {
     "merchantTransactionId": merchantTransactionId,
     "merchantUserId": 123,
     "amount": amountinrupee,
-    "redirectUrl": `https://dev.caterorange.com/redirect-url/${merchantTransactionId}?customer_id=${customer_id}&corporateorder_id=${corporateorder_id}`,
+    "redirectUrl": `https://dev.caterorange.com/api/redirect-url/${merchantTransactionId}?customer_id=${customer_id}&corporateorder_id=${corporateorder_id}`,
     "redirectMode": "REDIRECT",
     "callbackUrl": "https://webhook.site/callback-url",
     "mobileNumber": "9999999999",
