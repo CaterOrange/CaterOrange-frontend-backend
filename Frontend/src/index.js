f085befed7aa6b5f0910a41c539526bdc5490669
Sirisha-eng <sirisha@scaleorange.com>
2025-03-14 16:17:23 +0530

    Updated frontend to stop console messages in the browser console

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,18 +5,23 @@ import App from './App';
 import './index.css';
 import { CartProvider } from './services/contexts/CartContext';
 
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {};
+  console.debug = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+}
+console.log("Environment:", process.env.NODE_ENV);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="32217687275-jeco6hta0enblmg1vp45ums1q7n1gpra.apps.googleusercontent.com">
-    <CartProvider>
-      <App/>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <CartProvider>
+        <App />
       </CartProvider>
-      </GoogleOAuthProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
-
-
-// local 1064774169766-i5imbl507os608e821qspces1dhlofps.apps.googleusercontent.com
-
-//dev  239233180210-t0m8gf26n364e7m3ono4ij6lhp9rld9v.apps.googleusercontent.com
